a77f555fe6ea56aef81e8bd2a73909c5cb5860f6
romain <romain.lannes@gmail.com>
2018-02-16 18:29:00 +0100

    some work done on struct

--- src/read_write/from_blast.rs
+++ src/read_write/from_blast.rs
@@ -5,6 +5,11 @@ use std::path::Path;
 ///return hashmap, graph <edgesAtrr (min(cov), pid, eval), Nodeattr(name_real)>
 pub fn read_from_blast(header: &structure::DicoHeader, file: &Path, threshold_values:Options<EdgeAtrr>){
 
+	let my_graph = Graph::<NodeAttr, EdgesAttr, petgraph::Undirected>::new_undirected();
+        
+	let my_name_to_index_hashmap: FnvHashMap<String, petgraph::graph::NodeIndex> =
+        FnvHashMap::with_capacity_and_hasher(1_000_000, Default::default());
+        
    let in_file = File::open(path_in_file).unwrap_or_else(|why| {
 	panic!(
 		"couldn't open {}: {}",
@@ -23,11 +28,15 @@ pub fn read_from_blast(header: &structure::DicoHeader, file: &Path, threshold_va
 								.collect();
 		
 		let this_edges_properties = get_std_edges_attributs(&v_line, header);
-		// before going further we check if edges go
+		// before going further we check if edges pass threshold
 		match threshold_values{
-			some(threshold) => if !this_edges_properties.pass_threshold(threshold){continue}
+			some(threshold) => if !this_edges_properties.pass_threshold(threshold){continue},
 			}
 		
+		// get index return the graph index of a nodes , and create a node and add it to the graph if it don't exist 
+		let index1 = function.get_index(my_graph, my_name_to_index_hashmap, v_line[header.qid].to_string())
+		let index2 = function.get_index(my_graph, my_name_to_index_hashmap, v_line[header.qid].to_string())
+		// then if edges don't exist or is of lower quality update it
 		
 		}
 	}
