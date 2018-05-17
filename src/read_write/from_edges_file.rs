ba973e59cfb8acd31c64642e0c34dd9b0e956c52
romain <romain.lannes@gmail.com>
2018-05-17 11:11:33 +0100

    addedge_attr

--- src/read_write/from_edges_file.rs
+++ src/read_write/from_edges_file.rs
@@ -12,6 +12,48 @@ use std::path::Path;
 use petgraph::Graph;
 use fnv::FnvHashMap;
 
+
+pub fn read_from_EdgesAttr(file: &Path, threshold_values:Option<EdgesAttr>) ->
+	(FnvHashMap<String, petgraph::graph::NodeIndex>,
+    Graph<NodeAttr, LigthEdges, petgraph::Undirected>){
+
+	let mut my_graph = Graph::<NodeAttr, LigthEdges, petgraph::Undirected>::new_undirected();
+
+	let mut my_name_to_index_hashmap: FnvHashMap<String, petgraph::graph::NodeIndex> =
+        FnvHashMap::with_capacity_and_hasher(1_000_000, Default::default());
+
+   let in_file = File::open(file).unwrap_or_else(|why| {
+	panic!(
+		"couldn't open  {}",
+		file.display(),
+	)
+    });
+
+	let mut  in_file_buffer = BufReader::with_capacity(60_000, in_file); //bufering
+    for lines in in_file_buffer.lines(){
+
+		let current_line = lines.unwrap();
+		let v_line: Vec<_> = current_line.trim() // split line into vector
+								.split_whitespace()
+								.collect();
+
+		let mut default_weight: EdgesAttr;
+		default_weight = EdgesAttr{	eval: v_line[2].parse::<f64>().unwrap(),
+									pid: v_line[3].parse::<f32>().unwrap(),
+									cov: v_line[4].parse::<f32>().unwrap()};
+
+
+		let index1 = function::get_index(&mut my_graph, &mut my_name_to_index_hashmap,
+										 v_line[0].to_string(), NodeAttr{name_real: v_line[0].to_string()} );
+		let index2 = function::get_index(&mut my_graph, &mut my_name_to_index_hashmap,
+										 v_line[1].to_string(), NodeAttr{name_real: v_line[1].to_string()});
+		my_graph.update_edge(index1, index2, default_weight);
+
+		}
+		(my_name_to_index_hashmap, my_graph)
+}
+
+
 pub fn read_from_edges(file: &Path, threshold_values:Option<EdgesAttr>) -> 
 	(
 	FnvHashMap<String, petgraph::graph::NodeIndex>,
@@ -44,7 +86,7 @@ pub fn read_from_edges(file: &Path, threshold_values:Option<EdgesAttr>) ->
 			}
 			
 		else{
-			default_weight = LigthEdges{weight:v_line[3].parse::<f32>().unwrap()};
+			default_weight = LigthEdges{weight:v_line[2].parse::<f32>().unwrap()};
 			}
 			
 		let index1 = function::get_index(&mut my_graph, &mut my_name_to_index_hashmap,
